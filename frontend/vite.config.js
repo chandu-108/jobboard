e8059e02d9d0c4ac9a33fb785f615ad5d5a8f3d2
chandu-108 <chanduthota108@gmail.com>
2025-04-29 20:38:13 +0530

    last changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path'; // add this
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'), // this makes @ point to /src
+      '@': path.resolve(__dirname, './src'),
     },
+  },
+  build: {
+    outDir: "dist", // Ensure output directory is correct for deployment
+    emptyOutDir: true, // Clears the directory before each build
   },
 });
