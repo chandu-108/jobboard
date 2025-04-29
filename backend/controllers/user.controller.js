3365a36aaf84577e9208c105f0785f43678300d7
chandu-108 <chanduthota108@gmail.com>
2025-04-29 20:10:39 +0530

    changes

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -110,7 +110,7 @@ export const login = async (req, res) => {
       .cookie("token", token, {
         maxAge: 1 * 24 * 60 * 60 * 1000, // 1 day
         httpOnly: true,
-        sameSite: "strict",
+        sameSite: "none",
       })
       .json({
         message: `Welcome back ${user.fullname}`,
